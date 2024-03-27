0d07de5a7c77eae0a9080e65c5a7efe3c139f622
Cauã Oliveira <101019088+Caua-Oliveira@users.noreply.github.com>
2024-03-27 17:07:37 -0300

    added new config options

--- ModConfig.cs
+++ ModConfig.cs
@@ -1,14 +1,16 @@
-﻿using StardewModdingAPI.Utilities;
+﻿using StardewModdingAPI;
+using StardewModdingAPI.Utilities;
 
-namespace Sprinting
+namespace AutomateToolSwap
 {
     internal class ModConfig
     {
+        public bool Enabled { get; set; } = true;
+
+        public SButton ToggleKey { get; set; } = SButton.CapsLock;
+
         public bool Pickaxe_greater_wcan { get; set; } = false;
 
         public bool Pickaxe_over_melee { get; set; } = false;
-
-
-
     }
 }
