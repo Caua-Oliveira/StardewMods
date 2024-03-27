c58b6554676b8a23e12b9daccb9338a2c5382537
Cauã Oliveira <101019088+Caua-Oliveira@users.noreply.github.com>
2024-03-26 22:57:48 -0300

    first commit - config options variables

---
+++ ModConfig.cs
@@ -0,0 +1,14 @@
+﻿using StardewModdingAPI.Utilities;
+
+namespace Sprinting
+{
+    internal class ModConfig
+    {
+        public bool Pickaxe_greater_wcan { get; set; } = false;
+
+        public bool Pickaxe_over_melee { get; set; } = false;
+
+
+
+    }
+}
