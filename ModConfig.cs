54875e0afa781c65a8e6946eeaa341d68e4f8366
Cauã Oliveira <101019088+Caua-Oliveira@users.noreply.github.com>
2024-03-28 17:22:09 -0300

    added new options

--- ModConfig.cs
+++ ModConfig.cs
@@ -12,5 +12,9 @@ namespace AutomateToolSwap
         public bool Pickaxe_greater_wcan { get; set; } = false;
 
         public bool Pickaxe_over_melee { get; set; } = false;
+
+        public SButton LastToolButton { get; set; } = SButton.MouseMiddle;
+
+        public bool Hoe_in_empty_soil { get; set; } = true;
     }
 }
