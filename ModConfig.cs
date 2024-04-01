3ff655feabf4a5b5fde37c7f62758a768a17fbb6
Cauã Oliveira <101019088+Caua-Oliveira@users.noreply.github.com>
2024-03-31 21:44:40 -0300

    Added new options

--- ModConfig.cs
+++ ModConfig.cs
@@ -1,5 +1,4 @@
 ﻿using StardewModdingAPI;
-using StardewModdingAPI.Utilities;
 
 namespace AutomateToolSwap
 {
@@ -9,12 +8,16 @@ namespace AutomateToolSwap
 
         public SButton ToggleKey { get; set; } = SButton.CapsLock;
 
+        public SButton SwapKey { get; set; } = SButton.MouseLeft;
+
         public bool Pickaxe_greater_wcan { get; set; } = false;
 
         public bool Pickaxe_over_melee { get; set; } = false;
 
         public SButton LastToolButton { get; set; } = SButton.MouseMiddle;
-
+        
         public bool Hoe_in_empty_soil { get; set; } = true;
+
+        public bool Auto_switch_last_tool { get; set; } = false;
     }
 }
