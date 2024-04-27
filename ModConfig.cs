37b060b148a09977e7068671cc1fe386d65314a6
Cauã Oliveira <101019088+Caua-Oliveira@users.noreply.github.com>
2024-04-27 00:30:56 -0300

    added new options

--- ModConfig.cs
+++ ModConfig.cs
@@ -30,12 +30,19 @@ namespace AutomateToolSwap
 
         public bool HoeForEmptySoil { get; set; } = true;
 
+        public bool SeedForTilledDirt { get; set; } = true;
+
         public bool ScytheForGrass { get; set; } = false;
 
         public bool PickaxeOverWateringCan { get; set; } = false;
 
         public bool AnyToolForWeeds { get; set; } = false;
 
+        public bool SwapForSeedMaker { get; set; } = false;
+
+        public string SwapForKegs { get; set; } = "None";
+
+        public string SwapForPreservesJar { get; set; } = "None";
         public bool DisableTractorSwap { get; set; } = false;
     }
 }
