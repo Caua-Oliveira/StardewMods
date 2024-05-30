2520af803c6b94fe8a57aa03832ded4b1c8658e5
Cauã Oliveira <101019088+Caua-Oliveira@users.noreply.github.com>
2024-05-30 12:24:59 -0300

    Update ModEntry.cs

--- ModEntry.cs
+++ ModEntry.cs
@@ -16,20 +16,20 @@ namespace AutomateToolSwap
     {
         internal static ModEntry Instance { get; set; } = null!;
         internal static ModConfig Config { get; private set; } = null!;
-        internal static Check check { get; private set; } = null!;
+        internal static Check tileHas { get; private set; } = null!;
         internal static ITranslationHelper i18n;
         internal static bool isTractorModInstalled;
         internal static bool monsterNearby = false;
 
 
-        //When the mods are loading
-        //Quando os mods estão carregando
+        //Called when the mod is loading
+        //Chamada quando o mod está carregando
         public override void Entry(IModHelper helper)
         {
             Instance = this;
             i18n = Helper.Translation;
             Config = Helper.ReadConfig<ModConfig>();
-            check = new Check(Instance);
+            tileHas = new Check(Instance);
 
             helper.Events.GameLoop.GameLaunched += OnGameLaunched;
             helper.Events.Input.ButtonPressed += OnButtonPressed;
@@ -37,8 +37,8 @@ namespace AutomateToolSwap
 
         }
 
-        //When the game opes
-        //Quando o jogo abre
+        //Called when the game opens
+        //Chamada quando o jogo abre
         private void OnGameLaunched(object sender, GameLaunchedEventArgs e)
         {
             isTractorModInstalled = Helper.ModRegistry.IsLoaded("Pathoschild.TractorMod");
@@ -47,14 +47,101 @@ namespace AutomateToolSwap
 
         IndexSwitcher switcher = new IndexSwitcher(0);
 
-        //Special cases
-        //Casos especiais
+
+        //Called when a button is pressed
+        //Chamada quando um botão é pressionado
+        private void OnButtonPressed(object sender, ButtonPressedEventArgs e)
+        {
+            if (!Context.IsWorldReady || Game1.activeClickableMenu != null)
+                return;
+
+            // turns mod on/off
+            // desliga e liga o mod
+            if (Config.ToggleKey.JustPressed())
+            {
+                Config.Enabled = !Config.Enabled;
+                if (Config.Enabled)
+                    Game1.addHUDMessage(new HUDMessage("AutomateToolSwap " + i18n.Get("mod.Enabled"), 2));
+                else
+                    Game1.addHUDMessage(new HUDMessage("AutomateToolSwap " + i18n.Get("mod.Disabled"), 2));
+            }
+
+            // swaps to the last used item
+            // troca para o item usado mais recentemente
+            if (Config.LastToolKey.JustPressed() && Game1.player.canMove)
+                switcher.GoToLastIndex();
+
+
+            if (!ButtonMatched(e) || !Config.Enabled || !(Game1.player.canMove))
+                return;
+
+            // variables for the main method
+            // variáveis para o método principal
+            Farmer player = Game1.player;
+            GameLocation currentLocation = Game1.currentLocation;
+            ICursorPosition cursorPos = this.Helper.Input.GetCursorPosition();
+            Vector2 cursorTile = cursorPos.GrabTile;
+            Vector2 frontOfPlayerTile = new Vector2((int)Game1.player.GetToolLocation().X / Game1.tileSize, (int)Game1.player.GetToolLocation().Y / Game1.tileSize);
+
+            // different methods for detecting tiels
+            // diferentes métodos para detectar blocos
+            if (Config.DetectionMethod == "Cursor")
+                CheckTile(currentLocation, cursorTile, player);
+            else if (Config.DetectionMethod == "Player")
+                CheckTile(currentLocation, frontOfPlayerTile, player);
+
+        }
+
+        // detects what is in the tile that the player is looking at 
+        // detecta o que está no bloco que o jogador está olhando 
+        private void CheckTile(GameLocation location, Vector2 tile, Farmer player)
+        {
+            // code at OnUpdateTicked
+            // código em OnUpdateTicked
+            if (Config.AlternativeWeaponOnMonsters && player.CurrentItem is MeleeWeapon && !player.CurrentItem.Name.Contains("Scythe") && monsterNearby)
+                return;
+
+            // if the player is using slingshot, it will not be swapped because it is a long range weapon
+            // se o jogador estiver usando estilingue, não será trocado porque é uma arma de longo alcance
+            if (player.CurrentItem is Slingshot)
+                return;
+
+            // code at Check.cs
+            // código em Check.cs
+            if (tileHas.Objects(location, tile, player))
+                return;
+
+            if (tileHas.ResourceClumps(location, tile, player))
+                return;
+
+            if (tileHas.TerrainFeatures(location, tile, player))
+                return;
+
+            if (tileHas.Water(location, tile, player))
+                return;
+
+            if (Config.WeaponOnMonsters && !Config.AlternativeWeaponOnMonsters)
+                if (tileHas.Monsters(location, tile, player))
+                    return;
+
+            if (tileHas.Animals(location, tile, player))
+                return;
+
+            if (tileHas.DiggableSoil(location, tile, player))
+                return;
+
+        }
+
+
+        //Called when the game updates the tick (60 times per second)
+        //Chamada quando o jogo atualiza o tick (60 vezes por segundo)
         private void OnUpdateTicked(object sender, UpdateTickedEventArgs e)
         {
             if (!Context.IsWorldReady || Game1.activeClickableMenu != null)
                 return;
 
-            //Alternative "Weapon for Monsters"
+            //Alternative for the option "Weapon for Monsters"
+            //Alternativa para a opção "Arma para Monstros"
             if (Config.AlternativeWeaponOnMonsters && Config.WeaponOnMonsters)
             {
                 monsterNearby = true;
@@ -69,7 +156,7 @@ namespace AutomateToolSwap
 
                     if (monster.IsMonster && distance < Config.MonsterRangeDetection && Game1.player.canMove)
                     {
-                        if (check.Monsters(Game1.currentLocation, tile, Game1.player))
+                        if (tileHas.Monsters(Game1.currentLocation, tile, Game1.player))
                             return;
                     }
 
@@ -99,85 +186,9 @@ namespace AutomateToolSwap
             }
         }
 
-        private void OnButtonPressed(object sender, ButtonPressedEventArgs e)
-        {
-            // ignore if player hasn't loaded a save yet or is in a menu
-            // ignora se o jogador não tiver carregado um save ou estiver em um menu
-            if (!Context.IsWorldReady || Game1.activeClickableMenu != null)
-                return;
 
-            // turns mod on/off
-            // desliga e liga o mod
-            if (Config.ToggleKey.JustPressed())
-            {
-                Config.Enabled = !Config.Enabled;
-                if (Config.Enabled)
-                    Game1.addHUDMessage(new HUDMessage("AutomateToolSwap " + i18n.Get("mod.Enabled"), 2));
-                else
-                    Game1.addHUDMessage(new HUDMessage("AutomateToolSwap " + i18n.Get("mod.Disabled"), 2));
-            }
-
-            // swap to the last item
-            // troca para o item mais recente
-            if (Config.LastToolKey.JustPressed() && Game1.player.canMove)
-                switcher.GoToLastIndex();
-
-            // check if the mod should try to swap items
-            // verifica se o mod deve tentar trocar itens
-            if (!ButtonMatched(e) || !Config.Enabled || !(Game1.player.canMove))
-                return;
-
-            // variables for the main method
-            // variáveis para o método principal
-            Farmer player = Game1.player;
-            GameLocation currentLocation = Game1.currentLocation;
-            ICursorPosition cursorPos = this.Helper.Input.GetCursorPosition();
-            Vector2 cursorTile = cursorPos.GrabTile;
-            Vector2 frontOfPlayerTile = new Vector2((int)Game1.player.GetToolLocation().X / Game1.tileSize, (int)Game1.player.GetToolLocation().Y / Game1.tileSize);
-            if (Config.DetectionMethod == "Cursor")
-                CheckTile(currentLocation, cursorTile, player);
-
-            else if (Config.DetectionMethod == "Player")
-                CheckTile(currentLocation, frontOfPlayerTile, player);
-
-        }
-
-        // detects what is in the tile that the player is looking at and calls the function to swap items
-        // detecta o que está no bloco que o jogador está olhando e chama a função para trocar items
-        private void CheckTile(GameLocation location, Vector2 tile, Farmer player)
-        {
-            if (Config.AlternativeWeaponOnMonsters && player.CurrentItem is MeleeWeapon && !player.CurrentItem.Name.Contains("Scythe") && monsterNearby)
-                return;
-
-            if (player.CurrentItem is Slingshot)
-                return;
-
-            if (check.Objects(location, tile, player))
-                return;
-
-            if (check.ResourceClumps(location, tile, player))
-                return;
-
-            if (check.TerrainFeatures(location, tile, player))
-                return;
-
-            if (check.Water(location, tile, player))
-                return;
-
-            if (Config.WeaponOnMonsters && !Config.AlternativeWeaponOnMonsters)
-                if (check.Monsters(location, tile, player))
-                    return;
-
-            if (check.Animals(location, tile, player))
-                return;
-
-            if (check.DiggableSoil(location, tile, player))
-                return;
-
-        }
-
-        //Looks for the tool necessary for the action
-        //Procura pelo ferramenta necessária para a ação
+        //Looks for the index of the tool necessary for the action
+        //Procura pelo index da ferramenta necessária para a ação
         public void SetTool(Farmer player, Type toolType, string aux = "", bool anyTool = false)
         {
             switcher.canSwitch = Config.AutoReturnToLastTool;
@@ -238,8 +249,8 @@ namespace AutomateToolSwap
 
         }
 
-        //Looks for the item necessary for the action
-        //Procura pelo item necessário para a ação
+        //Looks for the index of the item necessary for the action
+        //Procura pelo index doitem necessário para a ação
         public void SetItem(Farmer player, string categorie, string item = "", string crops = "Both", int aux = 0)
         {
             switcher.canSwitch = Config.AutoReturnToLastTool;
