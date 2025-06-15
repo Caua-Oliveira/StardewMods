f98568e39611d954b55df96e44fdb05a09997f20
Cauã Oliveira <101019088+Caua-Oliveira@users.noreply.github.com>
2025-06-15 19:44:56 -0300

    update

---
+++ OnlyWhenItCounts/ItemExtensions.cs
@@ -0,0 +1,12 @@
+﻿
+/// <summary>The API that interacts with the objects from the mod Item Extensions.</summary>
+namespace OnlyWhenItCounts;
+public interface IItemExtensionsApi
+{
+    bool IsStone(string id);
+    bool IsResource(string id, out int? health, out string itemDropped);
+    bool IsClump(string qualifiedItemId);
+    bool HasBehavior(string qualifiedItemId, string target);
+    bool GetResourceData(string id, bool isClump, out object data);
+    bool GetBreakingTool(string id, bool isClump, out string tool);
+}
