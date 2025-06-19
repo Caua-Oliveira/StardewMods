19f8434fd9324ac536bbf8722226517ee6156140
Cauã Oliveira <101019088+Caua-Oliveira@users.noreply.github.com>
2025-06-19 18:52:36 -0300

    first commit

---
+++ MachinesDropItems/ModConfig.cs
@@ -0,0 +1,8 @@
+﻿namespace MachinesDropsItems
+{
+    public class ModConfig
+    {
+        public bool Enabled { get; set; } = true;
+        public int RetrievalTimeDivider { get; set; } = 1;
+    }
+}
